c4ccbae125b6a9f076e2b61be3490fa3f6cb0cbd
DevMisrty <dev.mistry.agileinfoways@gmail.com>
2025-12-16 13:32:11 +0530

    Initialize the project, empty project, with only folder and packages created

---
+++ src/test/java/com/practice/thoughtstream/ThoughtStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.thoughtstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
